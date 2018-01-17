5ab37239bfeac9564e2e0d1834ebfcd840cbf3ab
jason.zhang <douj27@gmail.com>
2018-01-17 16:05:20 +0800

    初步重构

--- src/index.js
+++ src/index.js
@@ -33,52 +33,19 @@ function Square(props){
 
 //棋盘
 class Board extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-        squares: Array(9).fill(null),
-        xIsNext: true,
-    };
-  }
-
   //value作为参数传值,通过this.props.value取值
   renderSquare(i) {
     //onClick 对应的是Square类中的props.onClick
     return <Square
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
+      value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)}
      />;
   }
 
-  handleClick(i){
-    //slice() 获取一个数组副本immutable，不是地址赋值
-    const squares = this.state.squares.slice();
-    //如果有人获胜，或者该方块被点击过。就忽略点击事件
-    if(calculateWinner(squares) || squares[i]){
-      return;
-    }
-    //相应位置的数组元素赋值X
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    //将修改过的数组副本赋给state.squares
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if(winner){
-      status = 'Winner: ' + winner;
-    }else{
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
-
     return (
       <div>
-        <div className="status">{status}</div>
+        {/* <div className="status">{status}</div> */}
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -101,14 +68,60 @@ class Board extends React.Component {
 
 //整个界面
 class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
+    };
+  }
+
+  handleClick(i){
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    //slice() 获取一个数组副本immutable，不是地址赋值
+    const squares = current.squares.slice();
+    //如果有人获胜，或者该方块被点击过。就忽略点击事件
+    if(calculateWinner(squares) || squares[i]){
+      return;
+    }
+    //相应位置的数组元素赋值X
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    //state状态中更新当前操作记录history
+    this.setState({
+      history: history.concat([{
+        squares: squares
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    //当前棋盘布局，是历史布局数组的最后一个元素
+    const current = history[history.length - 1];
+    //根据当前棋盘布局计算胜者
+    const winner = calculateWinner(current.squares);
+
+    let status;
+    if(winner){
+      status = 'Winner: ' + winner;
+    }else{
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
